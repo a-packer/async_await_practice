4bb344734f6d1879349af14a31e3f540d9465cc4
Aubrey <aubreypacker5@gmail.com>
2021-07-10 16:35:14 -0700

    added deck.js logic

--- deck.js
+++ deck.js
@@ -2,44 +2,39 @@
 let baseURL = 'https://deckofcardsapi.com/api/deck';
   
 // 1.
-$.getJSON(`${baseURL}/new/draw/`).then(data => {
-    let { suit, value } = data.cards[0];
+// $.getJSON(`${baseURL}/new/draw/`).then(data => {
+//     let { suit, value } = data.cards[0];
+//     console.log(`${value.toLowerCase()} of ${suit.toLowerCase()}`);
+// });
+async function getCard(deckId) {
+    res = await axios.get(`${baseURL}/${deckId}/draw/`)
+    let { suit, value } = res.data.cards[0];
     console.log(`${value.toLowerCase()} of ${suit.toLowerCase()}`);
-});
+  }
+  getCard("new")
 
 // 2.
-// Make a request to the deck of cards API to request a single card from a newly shuffled deck.
-// Once you have the card, make a request to the same API to get one more card from the same deck.
-// Once you have both cards, console.log the values and suits of both cards.
-$.getJSON(`${baseURL}/new/draw`)
-    .then(data => {
-        let card1 = data.cards[0];
-        let { suit, value } = card1;
-        console.log(`${value.toLowerCase()} of ${suit.toLowerCase()}`);
-        deck_id = data.deck_id;
-        return axios.get(`${baseURL}/${deck_id}/draw`);
-    })
-    .then(data => {
-        let card2 = data.cards[0];
-        let { suit, value } = card2;
-        console.log(`${value.toLowerCase()} of ${suit.toLowerCase()}`);
-    })
+async function get2Cards() {
+    let res1 = await axios.get(`${baseURL}/new/draw/`);
+    let card1 = res1.data.cards[0];
+    let { suit, value } = card1;
+    console.log(`${value.toLowerCase()} of ${suit.toLowerCase()}`);
+    getCard(res1.data.deck_id) // gets second card using above function
+  }
+  get2Cards()
 
 
 // 3. 
-$(function() {
-    let deckId = null;
+async function startup() {
     let $btn = $('button');
     let $deckDiv = $('#deck_div');
   
-    $.getJSON(`${baseURL}/new/shuffle/`)
-        .then(data => {
-            deckId = data.deck_id;
-    });
+    let deckInfo = await $.getJSON(`${baseURL}/new/shuffle/`)
+    let deckId = deckInfo.deck_id
   
-    $btn.on('click', function() {
-      $.getJSON(`${baseURL}/${deckId}/draw/`).then(data => {
-        let cardSrc = data.cards[0].image;
+    $btn.on('click', async function() {
+        let cardInfo = await $.getJSON(`${baseURL}/${deckId}/draw/`)
+        let cardSrc = cardInfo.cards[0].image;
         $deckDiv.append(
             $('<img>', {src: cardSrc, css: {
                     position: 'absolute',
@@ -47,6 +42,7 @@ $(function() {
                 }
             })
         );
-      });
-    });
-})
+
+    });  
+}    
+startup()
